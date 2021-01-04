c716ba792aa13d109d40a880c7d59ab99dc85897
Masora <masora@menhera.org>
2021-01-05 05:24:28 +0900

    updates

--- include.bashrc.sh
+++ include.bashrc.sh
@@ -1,13 +1,19 @@
 
 update_prompt () {
+	local status=$?
+	[ "$status" ] || status=0
+
+	local MEM_PERCENT
 	MEM_PERCENT=$(( 100 * $(
 		env -i LANG=C free -b | grep -i mem | sed 's/^[^0-9]*\([0-9]*\).*\s\([0-9]\{1,\}\)\s*$/\2 \/ \1/'
 	) ))
 
+	local CURRENT_MOUNTPOINT
 	which findmnt >/dev/null && {
 		CURRENT_MOUNTPOINT=$( findmnt -T "$PWD" | tail -n +2 | awk '{print $1}' )
 	} || CURRENT_MOUNTPOINT=/
 
+	local DISK_USAGE
 	DISK_USAGE=$(
 		env -i LANG=C df | tail -n +2 | sed 's/.*\s\([0-9]\{1,\}%\)\s*\(.*\)/\1 \2/' | while read disk_info
 		do
@@ -15,8 +21,16 @@ update_prompt () {
 			[ "$mountpoint" = "$CURRENT_MOUNTPOINT" ] && echo "${disk_info%% *}"
 		done
 	)
+	
+	local prefix
+	prefix="[\D{%H:%M:%S}] \u@\h M:${MEM_PERCENT}%, S:${DISK_USAGE}"
+	[ $status -eq 0 ] && {
+		suffix="\[\033[00m\]\[\033[42;39m\]\[\033[42;37m\] \$ \[\033[49;32m\]\[\033[00m\] "
+	} || {
+		suffix="\[\033[00m\]\[\033[41;39m\]\[\033[41;37m\] \$ \[\033[49;31m\]\[\033[00m\] "
+	}
 
-	PS1="[\D{%H:%M:%S}] \u@\h M:${MEM_PERCENT}%, S:${DISK_USAGE} \[\033[07m\] \W \[\033[00m\] "
+	PS1="${prefix} \[\033[07m\] \W ${suffix}"
 	export PS1
 }
 
